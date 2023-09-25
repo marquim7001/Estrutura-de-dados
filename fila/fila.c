4c982496ffa823a0ff9ea678887f5a3c92f4aa47
MARCOS ANTONIO SOARES DE SOUZA <marcos.souza@a.ucb.br>
2023-09-25 10:43:40 -0300

    elaborando estrutura de fila

---
+++ fila/fila.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fila.h"
+
+Fila* fila(){
+    Fila* F = (Fila*) malloc(sizeof(Fila))
+    F-> inicio = NULL;
+    F-> quantidade_objetos =0;
+    return F;
+}
+
+void enfileirar(Objeto* o, Fila* F){
+    if(F-> inicio ==NULL){
+        F-> inicio =o;
+    }else{
+        F -> final -> objeto_anterior =o;
+        F -> final =o;
+    }
+    F-> quantidade_objetos++;
+}
+
+
+Objeto* desenfileirar(Fila* F){
+    if(F -> quantidade_objetos==0){
+        return NULL;
+    }
+    Objeto* o= F ->inicio;
+    F -> inicio = F ->inicio-> objeto_anterior;
+    F-> quantidade_objetos--;
+    return o;
+}
